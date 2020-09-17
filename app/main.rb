77e948f096d1ebb9c1274c6b7c734ba60bbadbb2
danhealy <dan@beyondludus.com>
2020-09-16 22:48:32 -0700

    Example app uses README require dependency order

--- app/main.rb
+++ app/main.rb
@@ -1,29 +1,63 @@
+# No dependencies
 require 'lib/zif/zif.rb'
-require 'lib/zif/services/services.rb'
-require 'lib/zif/sprites/assignable.rb'
 require 'lib/zif/sprites/serializable.rb'
-require 'lib/zif/actions/action.rb'
-require 'lib/zif/actions/sequence.rb'
-require 'lib/zif/actions/actionable.rb'
-require 'lib/zif/actions/animatable.rb'
-require 'lib/zif/sprites/sprite.rb'
-require 'lib/zif/sprites/render_target.rb'
-require 'lib/zif/sprites/complex_sprite.rb'
-require 'lib/zif/services/sprite_registry.rb'
-require 'lib/zif/services/action_service.rb'
+require 'lib/zif/services/services.rb'
 require 'lib/zif/services/input_service.rb'
-require 'lib/zif/labels/label.rb'
-require 'lib/zif/components/two_stage_button.rb'
-require 'lib/zif/panels/nine_panel_edge.rb'
-require 'lib/zif/panels/nine_panel.rb'
-require 'lib/zif/scenes/scene.rb'
-require 'lib/zif/scenes/hud.rb'
-require 'lib/zif/camera.rb'
 require 'lib/zif/services/tick_trace_service.rb'
+require 'lib/zif/labels/label.rb'
+
+# Expects $services to be services.rb, works with tick_trace_service.rb
 require 'lib/zif/trace/traceable.rb'
+
+# Expects to be included in a Sprite subclass
+require 'lib/zif/sprites/assignable.rb'
+
+# Depends on serializable.rb
+require 'lib/zif/actions/action.rb'
+require 'lib/zif/scenes/scene.rb'
+
+# Depends on serializable.rb, action.rb
+require 'lib/zif/actions/sequence.rb'
+
+# Depends on action.rb, sequence.rb
+require 'lib/zif/actions/actionable.rb'
+
+# Depends on sequence.rb - expects an Actionable class
+require 'lib/zif/actions/animatable.rb'
+require 'lib/zif/services/action_service.rb'
+
+# Depends on serializable.rb, assignable.rb, actionable.rb, animatable.rb
+require 'lib/zif/sprites/sprite.rb'
+
+# Depends on sprite.rb, zif.rb
+require 'lib/zif/sprites/render_target.rb'
+
+# Depends on render_target.rb
+require 'lib/zif/sprites/complex_sprite.rb'
+
+# Depends on sprite.rb
+require 'lib/zif/services/sprite_registry.rb'
+require 'lib/zif/panels/nine_panel_edge.rb'
+
+# Depends on complex_sprite.rb
+require 'lib/zif/components/two_stage_button.rb'
+require 'lib/zif/panels/nine_panel.rb'
+
+# Depends on scene.rb, render_target.rb
+require 'lib/zif/scenes/hud.rb'
+
+# Depends on actionable.rb, zif.rb, - expects $game to be a game.rb
+require 'lib/zif/camera.rb'
+
+# Depends on render_target.rb, expects to be initialized with a LayeredTileMap-like @map
 require 'lib/zif/layered_tile_map/simple_layer.rb'
 require 'lib/zif/layered_tile_map/tiled_layer.rb'
+
+# Depends on simple_layer.rb, tiled_layer.rb, traceable.rb
 require 'lib/zif/layered_tile_map/layered_tile_map.rb'
+
+# Depends on traceable.rb, services.rb, input_service.rb, tick_trace_service.rb, action_service.rb, sprite_registry.rb,
+#            scene.rb
 require 'lib/zif/game.rb'
 
 require 'app/ui/labels/future_label.rb'
