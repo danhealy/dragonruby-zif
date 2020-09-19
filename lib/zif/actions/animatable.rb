9144d7713f43e4110655f5c89b8390806f634318
danhealy <dan@beyondludus.com>
2020-09-18 20:12:12 -0700

    Animatable: #stop_animating

--- lib/zif/actions/animatable.rb
+++ lib/zif/actions/animatable.rb
@@ -1,7 +1,7 @@
 module Zif
   # A mixin for an Actionable class which can set `#path=`
   module Animatable
-    attr_accessor :animation_sequences
+    attr_accessor :animation_sequences, :cur_animation
 
     # Creates an animation sequence based on an array of [[:path_name, ticks_to_hold]]
     def new_basic_animation(seq_name, paths_and_durations, repeat=:forever, &block)
@@ -20,10 +20,17 @@ module Zif
       raise ArgumentError, "No animation sequence named '#{seq_name}' registered" unless @animation_sequences[seq_name]
 
       # puts "Running animation sequence #{seq_name} #{@animation_sequences[seq_name].inspect}"
+      stop_animating
 
-      @animation_sequences[seq_name].cur_action.reset_duration
+      @cur_animation = seq_name
+      @animation_sequences[@cur_animation].cur_action.reset_duration
 
-      run(@animation_sequences[seq_name])
+      run(@animation_sequences[@cur_animation])
+    end
+
+    def stop_animating
+      return unless @cur_animation && @animation_sequences && @animation_sequences[@cur_animation]
+      stop_action(@animation_sequences[@cur_animation])
     end
   end
 end
